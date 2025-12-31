046d766915661a5968b8c5e72c80822e222ade2c
Vallab1 <baijnathprajapati140@gmail.com>
2025-12-31 11:13:48 +0530

    Completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    </HashRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+
+  </BrowserRouter>
 )
